03b3578e9abeef1815c93d233be095a5b70e27f6
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-07-23 22:49:51 +0530

    Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ let compScore = 0;
 
 let choices = document.querySelectorAll(".image")
 
+const genCompChoice = ()=>{
+    let options = ["rock","paper","scissor"]
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+}
+
 const playGame = (userChoice)=>{
 
-    console.log(userChoice);
+    console.log(`User choice = ${userChoice}`);
+    //Gen comp choice
+    const comChoice = genCompChoice();
+    console.log(`Computer choice = ${comChoice}`);
+    let msg = document.querySelector("#msg");
+    let msgBox = document.querySelector("#Message");
+    if(userChoice === comChoice){
+        console.log("GAME DRAW");
+        msg.innerText = "GAME DRAW";
+        msgBox.style.backgroundColor = "yellow";
+        msg.style.color = "black";
+    }
 }
 
 choices.forEach((choice)=>{
